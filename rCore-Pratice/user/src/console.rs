2749f52ae590058061ba4dbe9f00d8bfc323aec9
yicheng.lou <yicheng.lou@tronfuture.com>
2023-09-05 00:32:00 +0800

    minor update

---
+++ rCore-Pratice/user/src/console.rs
@@ -0,0 +1,17 @@
+use super::write;
+use core::fmt::{self, Write};
+
+
+const STDOUT: usize = 1;
+
+struct Stdout;
+impl Write for Stdout{
+    fn write_str(&mut self, s: &str) -> fmt::Result{
+        write(STDOUT, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments){
+    Stdout.write_fmt(args).unwrap();
+}
