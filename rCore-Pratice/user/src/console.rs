817f97752e207c463ebf7501e6c8de5e16749ae8
yicheng.lou <yicheng.lou@tronfuture.com>
2023-09-10 22:03:49 +0800

    add application

--- rCore-Pratice/user/src/console.rs
+++ rCore-Pratice/user/src/console.rs
@@ -15,3 +15,20 @@ impl Write for Stdout{
 pub fn print(args: fmt::Arguments){
     Stdout.write_fmt(args).unwrap();
 }
+
+/// print string macro
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+/// println string macro
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    
+    }
+}
---
+++ rCore-Pratice/user/src/lang_items.rs
@@ -0,0 +1,15 @@
+use core::panic::{self};
+#[panic_handler]
+fn panic(info: &panic::PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "[kernel] Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("[kernel] Panicked: {}", info.message().unwrap());
+    }
+    loop{}
+}
