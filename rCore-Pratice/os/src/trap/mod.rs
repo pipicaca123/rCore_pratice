19d3d9bdfd36e8fbe61252abb9f262ab81683ae2
yicheng.lou <yicheng.lou@tronfuture.com>
2023-09-16 09:03:49 +0800

    tmp

---
+++ rCore-Pratice/os/src/trap/mod.rs
@@ -0,0 +1,19 @@
+//! Trap handling functionality
+//!
+//! For rCore, we have a single trap entry point, namely `__alltraps`. At
+//! initialization in [`init()`], we set the `stvec` CSR to point to it.
+//!
+//! All traps go through `__alltraps`, which is defined in `trap.S`. The
+//! assembly language code does just enough work restore the kernel space
+//! context, ensuring that Rust code safely runs, and transfers control to
+//! [`trap_handler()`].
+//!
+//! It then calls different functionality based on what exactly the exception
+//! was. For example, timer interrupts trigger task preemption, and syscalls go
+//! to [`syscall()`].
+
+
+use core::arch::global_asm;
+use crate::syscall::syscall;
+
+global_asm!(include_str!("trap.S"));
